3e4e50a65305dfd3bcd63fc66b750af70860ed6b
alfannufi-bpma <app.support@bpma.go.id>
2026-03-11 11:37:49 +0700

    hot: adjust comments

--- src/Models/DataScope.php
+++ src/Models/DataScope.php
@@ -6,8 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class DataScope extends Model
 {
-    // Sesuaikan dengan nama tabel utama scope di package-mu
     protected $table = 'data_scopes'; 
     
-    protected $fillable = ['name', 'driver']; // Sesuaikan kolomnya
+    protected $fillable = ['name', 'driver'];
 }
